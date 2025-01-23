32e7a1a0b8242a157683908d00ea5a897e7c3be8
João <jloula@mit.edu>
2025-01-23 19:49:43 +0000

    feat: jitting split proposal, io

--- minijaxmix/infer.py
+++ minijaxmix/infer.py
@@ -3,16 +3,17 @@ import jax.numpy as jnp
 from functools import partial
 import numpy as np
 from jaxtyping import Array, Float, Bool
+import time
 
 def entropy(data):
     w = jnp.log(jnp.mean(data, axis=0))
     return -jnp.mean(jnp.sum(jnp.where(data, w, 0), axis=1))
 
-def clustering_entropy(clusters):
-    entropies = [entropy(cluster) for cluster in clusters]
-    p_y = np.array([cluster.shape[0] for cluster in clusters])
-    p_y = p_y / p_y.sum()
-    return sum(p_y * entropies) - sum(p_y * jnp.log(p_y))
+def conditional_entropy(data, c):
+    w = jnp.einsum('nc,nk->ck', c, data) / jnp.sum(c, axis=0)[..., None]
+    p_x_y = jnp.sum(jnp.where(data[:, None, :], jnp.log(w)[None, ...], 0), axis=-1)
+    res = - jnp.sum(jnp.where(c, c * p_x_y, 0), axis=0) / jnp.sum(c, axis=0)
+    return res
 
 def infer(key, data, n_splits, n_gibbs, n_branch=2, rejuvenation=True):
     clusters = [data]
@@ -21,6 +22,7 @@ def infer(key, data, n_splits, n_gibbs, n_branch=2, rejuvenation=True):
 
     for i in range(n_splits):
         key, subkey = jax.random.split(key)
+
         cs, cluster_entropies, total_entropies = zip(*[split_proposal(subkey, cluster, n_gibbs, n_segments=n_branch) for cluster in clusters])
         deltas = [old_entropy - new_entropy for old_entropy, new_entropy in zip(entropies, total_entropies)]
         best_idx = np.argmax(deltas)
@@ -31,8 +33,6 @@ def infer(key, data, n_splits, n_gibbs, n_branch=2, rejuvenation=True):
         entropies.pop(best_idx)
         entropies.extend(cluster_entropies[best_idx])
 
-        print("after split: ", clustering_entropy(clusters))
-
         # rejuvenation
         if rejuvenation:
             key, subkey = jax.random.split(key)
@@ -40,21 +40,31 @@ def infer(key, data, n_splits, n_gibbs, n_branch=2, rejuvenation=True):
             c = jnp.concatenate([k * jnp.ones(cluster.shape[0]).astype(jnp.int32) for k, cluster in enumerate(clusters)])
             p_y_x = jax.nn.one_hot(c, len(clusters))
 
+            conditional_entropies = conditional_entropy(concat_data, p_y_x)
+            p_y = jnp.mean(p_y_x, axis=0)
+            total_entropy = conditional_entropies @ p_y - jnp.sum(p_y * jnp.log(p_y))
+            print("entropy after split: ", total_entropy)
+
             def rejuvenation_step(p_y_x, _):
                 return gibbs_sampling(concat_data, p_y_x), None
 
             p_y_x, _ = jax.lax.scan(rejuvenation_step, jnp.log(p_y_x), length=n_gibbs)
             c = jax.random.categorical(key, p_y_x, axis=1)
-            clusters = split_data(concat_data, c)
-            entropies = [entropy(cluster) for cluster in clusters]
+            one_hot_c = jax.nn.one_hot(c, 2+i)
+            conditional_entropies = conditional_entropy(concat_data, one_hot_c)
+            p_y = jnp.mean(one_hot_c, axis=0)
+            total_entropy = conditional_entropies @ p_y - jnp.sum(p_y * jnp.log(p_y))
+            print("entropy after rejuvenation: ", total_entropy)
 
-            print("after rejuvenation: ", clustering_entropy(clusters))
+            clusters = split_data(concat_data, c)
+            entropies = list(conditional_entropies)
 
     return clusters, entropies
 
 def split_data(data, c):
     return [data[jnp.argwhere(c == k)][:, 0, :] for k in range(c.max() + 1)]
 
+@partial(jax.jit, static_argnames=("n_gibbs", "n_segments"))
 def split_proposal(key, data, n_gibbs, n_segments=2):
     N = data.shape[0]
     key, subkey = jax.random.split(key)
@@ -65,14 +75,22 @@ def split_proposal(key, data, n_gibbs, n_segments=2):
     def em_step(p_y_x, _):
         return gibbs_sampling(data, p_y_x), None
 
+    start_time = time.time()
     p_y_x, _ = jax.lax.scan(em_step, p_y_x, length=n_gibbs)
+    end_time = time.time()
+    print(f"em_step took {end_time - start_time} seconds")
     p_y = jnp.mean(jnp.exp(p_y_x), axis=0)
     c = jax.random.categorical(key, p_y_x, axis=1)
-    
-    clusters = split_data(data, c)
+    p_y_x = jax.nn.one_hot(c, n_segments)
 
-    cluster_entropies = [entropy(cluster) for cluster in clusters]
-    total_entropy = clustering_entropy(clusters)
+    # clusters = split_data(data, c)
+
+    start_time = time.time()
+    cluster_entropies = conditional_entropy(data, p_y_x)
+    p_y = jnp.mean(p_y_x, axis=0)
+    total_entropy = cluster_entropies @ p_y - jnp.sum(p_y * jnp.log(p_y))
+    end_time = time.time()
+    print(f"entropy took {end_time - start_time} seconds")
     return c, cluster_entropies, total_entropy
 
 def gibbs_sampling(data: Bool[Array, 'n k'], p_y_x_init: Float[Array, 'n c']):
---
+++ minijaxmix/io.py
@@ -0,0 +1,46 @@
+import polars as pl
+import numpy as np
+
+def dataframe_to_arrays(df: pl.DataFrame, n_bins: int = 20):
+    schema = make_schema(df)
+    categorical_df = df.select(schema["types"]["categorical"])
+    numerical_df = df.select(schema["types"]["piecewise_uniform"]).with_columns(
+        pl.all().qcut(quantiles=n_bins).name.keep()
+    )
+
+    categorical_idxs = np.concatenate(
+        [idx * np.ones(len(schema["var_metadata"][col]["levels"])) for idx, col in enumerate(categorical_df.columns)] +
+        [len(categorical_df.columns) + idx * np.ones(n_bins) for idx, col in enumerate(numerical_df.columns)]
+    )
+
+    df = pl.concat((categorical_df, numerical_df), how="horizontal")
+    return schema, df.to_dummies().to_numpy().astype(np.bool_), categorical_idxs
+
+def load_huggingface(dataset_path):
+    splits = {
+        "train": f"{dataset_path}/data-train-num.parquet",
+        "test": f"{dataset_path}/data-test-full-num.parquet"
+    }
+    train_df = pl.read_parquet(f"hf://datasets/Large-Population-Model/model-building-evaluation/{splits['train']}")
+    test_df = pl.read_parquet(f"hf://datasets/Large-Population-Model/model-building-evaluation/{splits['test']}")
+
+    return train_df, test_df
+
+def make_schema(df: pl.DataFrame):
+    schema = {
+        "types":{
+            "piecewise_uniform": [],
+            "normal": [],
+            "categorical": []
+        },
+        "var_metadata":{}
+    }
+    for c in df.columns:
+        if df[c].dtype == pl.Utf8:
+            schema["types"]["categorical"].append(c)
+            schema["var_metadata"][c] = {"levels": df[c].drop_nulls().unique().sort().to_list()}
+        elif df[c].dtype == pl.Float64:
+            schema["types"]["piecewise_uniform"].append(c)
+        else:
+            raise ValueError(c)
+    return schema
