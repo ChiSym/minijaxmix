8bd9b726dc405b2f9c20730783a80c7f6233fd28
João <jloula@mit.edu>
2025-01-24 19:55:40 +0000

    feat: jitting all of infer

--- minijaxmix/infer.py
+++ minijaxmix/infer.py
@@ -1,4 +1,8 @@
 import jax
+jax.config.update("jax_compilation_cache_dir", "jax_cache")
+jax.config.update("jax_persistent_cache_min_entry_size_bytes", -1)
+jax.config.update("jax_persistent_cache_min_compile_time_secs", 0)
+
 import jax.numpy as jnp
 from functools import partial
 import numpy as np
@@ -20,21 +24,15 @@ def conditional_entropy(data, c):
 @partial(jax.jit, static_argnames=("n_splits", "n_gibbs", "n_categories", "n_branch", "rejuvenation", "minibatch_size"))
 def infer(key, data, n_splits, n_gibbs, categorical_idxs, n_categories, n_branch=2, rejuvenation=True, minibatch_size=1000):
     N = data.shape[0]
-    logp_x_y = jnp.log(jnp.ones((N, 1)) / N)
     p_ys = jnp.array([1.])
     ws = jnp.array([jnp.mean(data, axis=0)])
+    assignments = jnp.zeros(N, dtype=jnp.int32)
     conditional_entropies = jnp.array([entropy(data)])
-    jax.debug.print("initial entropy: {conditional_entropies}", conditional_entropies=conditional_entropies[0])
 
-
-    def infer_step(step_key, i, logp_x_y, p_ys, ws, conditional_entropies):
+    def infer_step(step_key, minibatches, p_ys, ws, conditional_entropies):
         key1, key2 = jax.random.split(step_key)
-        keys = jax.random.split(key2, 1 + i)
-        p_x_y = jnp.exp(logp_x_y - jax.nn.logsumexp(logp_x_y, axis=0))
-        minibatches = jax.vmap(jax.random.choice, in_axes=(0, None, None, None, 0))(keys, data, (minibatch_size,), True, p_x_y.T)
-
-        key1, key2 = jax.random.split(key1)
-        keys = jax.random.split(key2, 1 + i)
+        c = len(minibatches)
+        keys = jax.random.split(key2, c)
         cluster_p_ys, cluster_ws, cluster_entropies, total_entropies = jax.vmap(split_proposal, in_axes=(0, 0, None, None, None, None))(
             keys, minibatches, n_gibbs, categorical_idxs, n_categories, n_branch)
 
@@ -43,22 +41,23 @@ def infer(key, data, n_splits, n_gibbs, categorical_idxs, n_categories, n_branch
 
         p_y = cluster_p_ys[best_idx]
         w = cluster_ws[best_idx]
-
-        conditional_entropies = jnp.delete(conditional_entropies, best_idx, axis=0, assume_unique_indices=True)
-        conditional_entropies = jnp.concatenate((conditional_entropies, cluster_entropies[best_idx]), axis=0)
+        split_entropy = cluster_entropies[best_idx]
 
         prev_p_y = p_ys[best_idx]
         p_ys = jnp.delete(p_ys, best_idx, axis=0, assume_unique_indices=True)
-        p_ys = jnp.concatenate((p_ys, prev_p_y * cluster_p_ys[best_idx]), axis=0)
+        p_ys = jnp.concatenate((p_ys, prev_p_y * p_y), axis=0)
 
         ws = jnp.delete(ws, best_idx, axis=0, assume_unique_indices=True)
-        ws = jnp.concatenate((ws, cluster_ws[best_idx]), axis=0)
+        ws = jnp.concatenate((ws, w), axis=0)
+
+        conditional_entropies = jnp.delete(conditional_entropies, best_idx, axis=0, assume_unique_indices=True)
+        conditional_entropies = jnp.concatenate((conditional_entropies, split_entropy), axis=0)
 
         logp_x_y = update_logp_x_y(data, ws)
         logp_y_x = update_logp_y_x(p_ys, logp_x_y)
 
         total_entropy = conditional_entropies @ p_ys - jnp.sum(p_ys * jnp.log(p_ys))
-        jax.debug.print("entropy: {total_entropy}", total_entropy=total_entropy)
+        # jax.debug.print("entropy: {total_entropy}", total_entropy=total_entropy)
 
         if rejuvenation:
             def rejuvenation_step(p_y_w, key):
@@ -72,17 +71,37 @@ def infer(key, data, n_splits, n_gibbs, categorical_idxs, n_categories, n_branch
 
             conditional_entropies = conditional_entropy(data, jnp.exp(logp_y_x))
             total_entropy = conditional_entropies @ p_ys - jnp.sum(p_ys * jnp.log(p_ys))
-            jax.debug.print("entropy after rejuvenation: {total_entropy}", total_entropy=total_entropy)
+            # jax.debug.print("entropy after rejuvenation: {total_entropy}", total_entropy=total_entropy)
 
-        return (logp_x_y, p_ys, ws, conditional_entropies), None
+        # hard clustering
+        key1, key2 = jax.random.split(key1)
+        assignments = jax.random.categorical(key1, logp_y_x, axis=1)
+        p_y_x = jax.nn.one_hot(assignments, num_classes=c + 1)
+        logp_y_x = jnp.log(p_y_x)
+        p_y = update_p_y(logp_y_x)
+        w = update_w(key2, data, logp_y_x, categorical_idxs, n_categories)
+        conditional_entropies = conditional_entropy(data, p_y_x)
+        total_entropy = conditional_entropies @ p_y - jnp.sum(p_y * jnp.log(p_y))
+
+        # jax.debug.print("entropy after sampling assignments: {total_entropy}", total_entropy=total_entropy)
+
+        return p_y, w, conditional_entropies, assignments
 
     keys = jax.random.split(key, n_splits)
     # we could use lax.scan here, but at the cost of padding each step to the max number of clusters
     for i in range(n_splits):
         key = keys[i]
-        (logp_x_y, p_ys, ws, conditional_entropies), _ = infer_step(key, i, logp_x_y, p_ys, ws, conditional_entropies)
+        key, subkey = jax.random.split(key)
+        minibatches = make_minibatches(subkey, data, assignments, i + 1, minibatch_size)
 
-    return logp_x_y, p_ys, ws, conditional_entropies
+        p_ys, ws, conditional_entropies, assignments = infer_step(key, minibatches, p_ys, ws, conditional_entropies)
+
+    return p_ys, ws, conditional_entropies
+
+def make_minibatches(key, data, c, num_clusters, minibatch_size):
+    keys = jax.random.split(key, num_clusters)
+    clusters = jax.vmap(jax.random.choice, in_axes=(0, None, None, None, 0))(keys, data, (minibatch_size,), True, (c[None, :] == jnp.arange(num_clusters)[:, None]) / jnp.sum(c[None, :] == jnp.arange(num_clusters)[:, None], axis=1)[:, None])
+    return clusters
 
 @partial(jax.jit, static_argnames=("n_gibbs", "n_segments", "n_categories"))
 def split_proposal(key, data, n_gibbs, categorical_idxs: Integer[Array, "k"], n_categories: int, n_segments=2):
@@ -100,10 +119,8 @@ def split_proposal(key, data, n_gibbs, categorical_idxs: Integer[Array, "k"], n_
 
     key, subkey = jax.random.split(key)
     keys = jax.random.split(subkey, n_gibbs)
-    start_time = time.time()
     (p_y, w), _ = jax.lax.scan(em_step, (p_y, w), keys)
-    end_time = time.time()
-    print(f"em_step took {end_time - start_time} seconds")
+    # print(f"em_step took {end_time - start_time} seconds")
 
     logp_x_y = update_logp_x_y(data, w)
     logp_y_x = update_logp_y_x(p_y, logp_x_y)
